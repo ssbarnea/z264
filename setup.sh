5a16d9d51e3ab787c52f6876896c2eda93e855fa
sorin.sbarnea@gmail.com <sorin.sbarnea@gmail.com>
2014-06-23 20:22:28 +0100

    Removed unecessarly complex requirements installtion procedures and replaced them with easy brew install ones.

--- setup.sh
+++ setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-brew install ffmpeg mkvtomp4
+set -ex
+brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
+brew install caskroom/cask/brew-cask
+brew install ffmpeg mkvtomp4 mediainfo wget rmtrash p7zip
+brew cask install handbrake
