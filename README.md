7110442d6a64b6563f2f0c4d96e720d77a3b62f7
sorin.sbarnea@gmail.com <sorin.sbarnea@gmail.com>
2014-06-24 18:47:25 +0100

    Added first set of mkv tests, now failing on 8/10.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 z264
 ====
+
+Testing files from:
+
+http://www.auby.no/files/video_tests/
--- setup.sh
+++ setup.sh
@@ -4,3 +4,8 @@ brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup
 brew install caskroom/cask/brew-cask
 brew install ffmpeg mkvtomp4 mediainfo wget rmtrash p7zip
 brew cask install handbrake
+
+wget --timestamp http://subler.googlecode.com/files/SublerCLI-0.19.zip >/dev/null
+unzip -o SublerCLI-0.19.zip >/dev/null
+mv -n SublerCLI "$HOME/bin/"
+chmod 755 "$HOME/bin/SublerCLI"
